626e4e9a773d9ae9dabe3c8d51efa900ddb6b29f
James Mwendwa <mwendwajames32@gmail.com>
2022-11-21 13:53:55 +0300

    products json file

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,7 +1,177 @@
-export const items = [
+export const diningItems = [
+  {
+    id: 6,
+    img: "/images/img6.jpg",
+    title: "",
+    price: 0,
+  },
+
+  {
+    id: 10,
+    img: "/images/img10.jpg",
+    title: "",
+    price: 0,
+  },
+
+  {
+    id: 15,
+    img: "/images/img15.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 16,
+    img: "/images/img16.jpg",
+    title: "",
+    price: 0,
+  },
+
+  {
+    id: 19,
+    img: "/images/img19.jpg",
+    title: "",
+    price: 0,
+  },
+
+  {
+    id: 21,
+    img: "/images/img21.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 22,
+    img: "/images/img22.jpg",
+    title: "",
+    price: 0,
+  },
+];
+
+export const officeItems = [
+  {
+    id: 23,
+    img: "/images/img23.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 2,
+    img: "/images/img2.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 3,
+    img: "/images/img3.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 4,
+    img: "/images/img4.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 13,
+    img: "/images/img13.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 14,
+    img: "/images/img14.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 17,
+    img: "/images/img17.jpg",
+    title: "",
+    price: 0,
+  },
+
+  {
+    id: 27,
+    img: "/images/img27.jpg",
+    title: "",
+    price: 0,
+  },
+];
+
+export const sofaItems = [
+  {
+    id: 25,
+    img: "/images/img25.jpg",
+    title: "",
+    price: 0,
+  },
   {
     id: 1,
     img: "/images/img1.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 26,
+    img: "/images/img26.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 18,
+    img: "/images/img18.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 11,
+    img: "/images/img11.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 8,
+    img: "/images/img8.jpg",
+    title: "",
+    price: 0,
+  },
+];
+
+export const bedroomItems = [
+  {
+    id: 9,
+    img: "/images/img9.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 5,
+    img: "/images/img5.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 7,
+    img: "/images/img7.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 12,
+    img: "/images/img12.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 20,
+    img: "/images/img20.jpg",
+    title: "",
+    price: 0,
+  },
+  {
+    id: 24,
+    img: "/images/img24.jpg",
     title: "",
     price: 0,
   },
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
+import { diningItems as items } from "../lib/data";
+
 export const Home = () => {
   return (
     <div>
-      <h2>Home Page</h2>
+      {items.map((item) => {
+        return (
+          <div className="card" key={item.id}>
+            <img src={item.img} alt={item.img} />
+            <h1>{item.id}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 };

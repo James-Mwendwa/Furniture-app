556be8f41c244c0931fc25c21796f1c124e3909f
James Mwendwa <mwendwajames32@gmail.com>
2022-12-03 23:17:18 +0300

    modify json file

--- src/lib/data.js
+++ src/lib/data.js
@@ -2,48 +2,28 @@ export const diningItems = [
   {
     id: 6,
     img: "/images/img6.jpg",
-    title: "",
-    price: 0,
+    desc: "Kitchen set 7 pieces",
+    price: 2990,
   },
 
   {
     id: 10,
     img: "/images/img10.jpg",
-    title: "",
-    price: 0,
+    desc: "Furniture Dinette set 6",
+    price: 2499,
   },
 
   {
     id: 15,
     img: "/images/img15.jpg",
-    title: "",
-    price: 0,
+    desc: "Round Dining Table and 4 chairs",
+    price: 1999,
   },
   {
     id: 16,
     img: "/images/img16.jpg",
-    title: "",
-    price: 0,
-  },
-
-  {
-    id: 19,
-    img: "/images/img19.jpg",
-    title: "",
-    price: 0,
-  },
-
-  {
-    id: 21,
-    img: "/images/img21.jpg",
-    title: "",
-    price: 0,
-  },
-  {
-    id: 22,
-    img: "/images/img22.jpg",
-    title: "",
-    price: 0,
+    desc: "East West Furniture",
+    price: 999,
   },
 ];
 
@@ -51,51 +31,27 @@ export const officeItems = [
   {
     id: 23,
     img: "/images/img23.jpg",
-    title: "",
-    price: 0,
+    desc: "Adjustable standing desk",
+    price: 699,
   },
   {
     id: 2,
     img: "/images/img2.jpg",
-    title: "",
-    price: 0,
+    desc: "StarTech Workstation",
+    price: 259,
   },
-  {
-    id: 3,
-    img: "/images/img3.jpg",
-    title: "",
-    price: 0,
-  },
-  {
-    id: 4,
-    img: "/images/img4.jpg",
-    title: "",
-    price: 0,
-  },
-  {
-    id: 13,
-    img: "/images/img13.jpg",
-    title: "",
-    price: 0,
-  },
+
   {
     id: 14,
     img: "/images/img14.jpg",
-    title: "",
-    price: 0,
+    desc: "Portable standing Desk",
+    price: 654,
   },
   {
     id: 17,
     img: "/images/img17.jpg",
-    title: "",
-    price: 0,
-  },
-
-  {
-    id: 27,
-    img: "/images/img27.jpg",
-    title: "",
-    price: 0,
+    desc: "Boardroom tables and chairs",
+    price: 1500,
   },
 ];
 
@@ -103,38 +59,27 @@ export const sofaItems = [
   {
     id: 25,
     img: "/images/img25.jpg",
-    title: "",
-    price: 0,
+    desc: "",
+    price: 1200,
   },
   {
     id: 1,
     img: "/images/img1.jpg",
-    title: "",
-    price: 0,
+    desc: "",
+    price: 1100,
   },
-  {
-    id: 26,
-    img: "/images/img26.jpg",
-    title: "",
-    price: 0,
-  },
+
   {
     id: 18,
     img: "/images/img18.jpg",
-    title: "",
-    price: 0,
+    desc: "",
+    price: 1400,
   },
   {
     id: 11,
     img: "/images/img11.jpg",
-    title: "",
-    price: 0,
-  },
-  {
-    id: 8,
-    img: "/images/img8.jpg",
-    title: "",
-    price: 0,
+    desc: "",
+    price: 1200,
   },
 ];
 
@@ -142,37 +87,27 @@ export const bedroomItems = [
   {
     id: 9,
     img: "/images/img9.jpg",
-    title: "",
-    price: 0,
+    desc: "",
+    price: 3500,
   },
   {
     id: 5,
     img: "/images/img5.jpg",
-    title: "",
-    price: 0,
+    desc: "",
+    price: 3000,
   },
-  {
-    id: 7,
-    img: "/images/img7.jpg",
-    title: "",
-    price: 0,
-  },
+
   {
     id: 12,
     img: "/images/img12.jpg",
-    title: "",
-    price: 0,
+    desc: "",
+    price: 3200,
   },
-  {
-    id: 20,
-    img: "/images/img20.jpg",
-    title: "",
-    price: 0,
-  },
+
   {
     id: 24,
     img: "/images/img24.jpg",
-    title: "",
-    price: 0,
+    desc: "",
+    price: 2700,
   },
 ];

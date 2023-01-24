6435a41fc9882bbb7dbcef3a3442f9663b5d741a
James Mwendwa <mwendwajames32@gmail.com>
2023-01-24 21:18:23 +0300

    display data

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -11,7 +11,7 @@ type DataProps = {
 };
 
 const Catalog: React.FC = () => {
-  const [data, setData] = useState<[DataProps | null]>([null]);
+  const [data, setData] = useState<Array<DataProps[]>>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,7 +31,7 @@ const Catalog: React.FC = () => {
       ];
 
       const officeData = await Promise.all(promises);
-      setData(data);
+      setData(officeData);
     };
 
     fetchData();
@@ -40,47 +40,113 @@ const Catalog: React.FC = () => {
   return (
     <div className="bg-bg">
       <div className="p-5 mx-10">
-        <div className="flex items-center justify-between">
-          <h2 className="text-xl">Dinning</h2>
-          <BsFillArrowRightCircleFill className="text-2xl cursor-pointer" />
+        <div className="flex items-center justify-between  bg-white rounded-lg">
+          <h2 className="text-xl ml-4">Dinning</h2>
+          <BsFillArrowRightCircleFill className="text-2xl cursor-pointer mr-2" />
         </div>
-        {data[0] && (
-          <div className="grid grid-cols-4 gap-3 cursor-pointer">
-            <LazyLoadImage src={data[0]["img"]} />
-            <h2>{data[0].desc}</h2>
-            <p>${data[0].price}</p>
-            <button
-              onClick={() => console.log("item added")}
-              className="bg-yellow-500 mt-3 text-white"
-            >
-              ADD TO CART
-            </button>
-          </div>
-        )}
+
+        <div className="grid grid-cols-4 gap-2 cursor-pointer bg-white">
+          {data[0] &&
+            data[0].map((item) => (
+              <div key={item.id} className="m-4">
+                <LazyLoadImage
+                  className="w-80 h-40 rounded-xl mb-2"
+                  src={item.img}
+                  alt=""
+                />
+                <h2>{item.desc}</h2>
+                <p>${item.price}</p>
+                <button
+                  onClick={() => alert("item added")}
+                  className="bg-yellow-500 p-1 text-sm rounded-md mt-3 text-white"
+                >
+                  ADD TO CART
+                </button>
+              </div>
+            ))}
+        </div>
       </div>
 
       <div className="p-5 mx-10">
-        <div className="flex items-center justify-between">
-          <h2 className="text-xl">Office</h2>
-          <BsFillArrowRightCircleFill className="text-2xl cursor-pointer" />
+        <div className="flex items-center justify-between  bg-white rounded-lg">
+          <h2 className="text-xl ml-4">Office</h2>
+          <BsFillArrowRightCircleFill className="text-2xl cursor-pointer mr-4" />
         </div>
-        <div className="grid grid-cols-4 gap-3 cursor-pointer"></div>
+
+        <div className="grid grid-cols-4 gap-3 cursor-pointer bg-white">
+          {data[0] &&
+            data[1].map((item) => (
+              <div className="m-4" key={item.id}>
+                <LazyLoadImage
+                  className="w-80 h-40 rounded-xl mb-2"
+                  src={item.img}
+                  alt=""
+                />
+                <h2>{item.desc}</h2>
+                <p>${item.price}</p>
+                <button
+                  onClick={() => alert("item added")}
+                  className="bg-yellow-500 p-1 text-sm rounded-md mt-3 text-white"
+                >
+                  ADD TO CART
+                </button>
+              </div>
+            ))}
+        </div>
       </div>
 
       <div className="p-5 mx-10">
-        <div className="flex items-center justify-between">
-          <h2 className="text-xl">Sofa</h2>
-          <BsFillArrowRightCircleFill className="text-2xl cursor-pointer" />
+        <div className="flex items-center justify-between  bg-white rounded-lg">
+          <h2 className="text-xl ml-4">Sofa</h2>
+          <BsFillArrowRightCircleFill className="text-2xl cursor-pointer mr-4" />
         </div>
-        <div className="grid grid-cols-4 gap-3 cursor-pointer"></div>
+        <div className="grid grid-cols-4 gap-3 cursor-pointer  bg-white">
+          {data[0] &&
+            data[2].map((item) => (
+              <div className="m-4" key={item.id}>
+                <LazyLoadImage
+                  className="w-80 h-40 rounded-xl mb-2"
+                  src={item.img}
+                  alt=""
+                />
+                <h2>{item.desc}</h2>
+                <p>${item.price}</p>
+                <button
+                  onClick={() => alert("item added")}
+                  className="bg-yellow-500 p-1 text-sm rounded-md mt-3 text-white"
+                >
+                  ADD TO CART
+                </button>
+              </div>
+            ))}
+        </div>
       </div>
 
       <div className="p-5 mx-10">
-        <div className="flex items-center justify-between">
-          <h2 className="text-xl">Bedroom</h2>
-          <BsFillArrowRightCircleFill className="text-2xl cursor-pointer" />
+        <div className="flex items-center justify-between bg-white rounded-lg">
+          <h2 className="text-xl ml-4">Bedroom</h2>
+          <BsFillArrowRightCircleFill className="text-2xl cursor-pointer mr-2" />
         </div>
-        <div className="grid grid-cols-4 gap-3 cursor-pointer"></div>
+        <div className="grid grid-cols-4 gap-3 cursor-pointer rounded-md  bg-white">
+          {data[0] &&
+            data[3].map((item) => (
+              <div className="m-4" key={item.id}>
+                <LazyLoadImage
+                  className="w-80 h-40 rounded-xl mb-2"
+                  src={item.img}
+                  alt=""
+                />
+                <h2>{item.desc}</h2>
+                <p>${item.price}</p>
+                <button
+                  onClick={() => alert("item added")}
+                  className="bg-yellow-500 p-1 text-sm rounded-md mt-3 text-white"
+                >
+                  ADD TO CART
+                </button>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );

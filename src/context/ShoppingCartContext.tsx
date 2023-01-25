06a3f86f7b179d36f74c8bc23fbc860c59b33508
James Mwendwa <mwendwajames32@gmail.com>
2023-01-25 22:42:40 +0300

    feat(context api): create shopping cart context

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, ReactNode, useState } from "react";
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+type ShoppingCartContext = {};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export const ShoppingCartProvider = ({
+  children,
+}: ShoppingCartProviderProps) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  return { children };
+};

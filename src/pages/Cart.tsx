be4a136668c93042782e0ffe5d33f4f491872108
James Mwendwa <mwendwajames32@gmail.com>
2023-02-10 22:47:46 +0300

    add cart item

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -10,19 +10,25 @@ const Cart = () => {
   } = useContext(ShoppingCartContext);
 
   return (
-    <>
-      {cartItems.map((item) => {
-        return (
-          <>
-            <div key={item.id}>
-              <img src={item.img} alt="" />
-              <h2>{item.price}</h2>
-              <h2>{item.desc}</h2>
+    <div className="bg-bg p-5">
+      {cartItems.map((item) => (
+        <div className="m-4 bg-white" key={item.id}>
+          <img className="w-60 h-30 rounded-xl mb-2" src={item.img} alt="" />
+          <h2>{item.price}</h2>
+          <h2>{item.desc}</h2>
+          <div className="flex items-center justify-between">
+            <button onClick={() => removeFromCart(item.id)}>REMOVE</button>
+            <div className="flex">
+              <button onClick={() => increaseCartQuantity(item.id, item)}>
+                -
+              </button>
+
+              <button onClick={() => decreaseCartQuantity(item.id)}>+</button>
             </div>
-          </>
-        );
-      })}
-    </>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
